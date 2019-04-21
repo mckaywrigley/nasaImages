58884f5525f07f7a3e372ac0a88a937c82e275b9
Mckay <mckaywrigley@gmail.com>
2019-04-20 19:12:24 -0700

    axios call working to unsplash

--- nasa/src/actions/index.js
+++ nasa/src/actions/index.js
@@ -2,11 +2,14 @@ import axios from "axios";
 
 import { IMAGE_DATA } from "./types";
 
-const url = "https://images-api.nasa.gov";
+const url = "https://api.unsplash.com";
 
 export const getImages = searchTerm => dispatch => {
   axios
-    .get(`${url}/search/${searchTerm}`)
+    .get(
+      `${url}/search/photos?client_id=899e9f3093d193dab9b2b84879e0522d33f639c098550a874f92560e492679c4`,
+      { params: { query: searchTerm } }
+    )
     .then(res => {
       dispatch({
         type: IMAGE_DATA,
--- nasa/src/components/SearchBar.js
+++ nasa/src/components/SearchBar.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+import { getImages } from "../actions";
+
 class SearchBar extends Component {
   constructor() {
     super();
@@ -7,6 +10,19 @@ class SearchBar extends Component {
       search: ""
     };
   }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = term => {
+    this.props.getImages(term);
+    this.setState({
+      search: ""
+    });
+  };
 
   render() {
     return (
@@ -17,10 +33,21 @@ class SearchBar extends Component {
           name="search"
           value={this.state.search}
           placeholder="search"
+          onChange={this.handleChange}
         />
+        <button onClick={e => this.handleSubmit(this.state.search)}>
+          Search
+        </button>
       </div>
     );
   }
 }
 
-export default SearchBar;
+const mapStateToProps = state => ({
+  images: state.nasa
+});
+
+export default connect(
+  mapStateToProps,
+  { getImages }
+)(SearchBar);
